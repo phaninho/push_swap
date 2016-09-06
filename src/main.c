7c465fb2913e5dd750f7d6b16037ed34c9cce99f
Stephane MARTINS <stmartin@e1r10p16.42.fr>
2016-09-06 15:45:00 +0200

    update push swap, can read letter in stdin

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/05 12:19:33 by stmartin          #+#    #+#             */
-/*   Updated: 2016/09/05 16:55:03 by stmartin         ###   ########.fr       */
+/*   Updated: 2016/09/06 15:43:51 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,48 @@
 
 int			check_instruction(char *str)
 {
-	if (ft_strcmp(str, "sa"))
-		ft_putendl("ins Error");
-	return (1);
+	if (ft_strlen(str) > 4)
+	{
+		ft_putendl("Error");
+		return (1);
+	}
+	else if (!(ft_strcmp(str, "sa\n")))
+		ft_putendl("entree correct");
+	else if (!(ft_strcmp(str, "sb\n")))
+		ft_putendl("entree correct");
+	else if (!(ft_strcmp(str, "ss\n")))
+		ft_putendl("entree correct");
+	else if (!(ft_strcmp(str, "pa\n")))
+		ft_putendl("entree correct");
+	else if (!(ft_strcmp(str, "pb\n")))
+		ft_putendl("entree correct");
+	else if (!(ft_strcmp(str, "ra\n")))
+		ft_putendl("entree correct");
+	else if (!(ft_strcmp(str, "rb\n")))
+		ft_putendl("entree correct");
+	else if (!(ft_strcmp(str, "rr\n")))
+		ft_putendl("entree correct");
+	else if (!(ft_strcmp(str, "rra\n")))
+		ft_putendl("entree correct");
+	else if (!(ft_strcmp(str, "rrb\n")))
+		ft_putendl("entree correct");
+	else if (!(ft_strcmp(str, "rrr\n")))
+		ft_putendl("entree correct");
+	else
+	{
+		ft_putstr("mauvais entree\n");
+		return (1);
+	}
+	return (0);
 }
 
 void		call_fctn(char **av)
 {
 	(void)av;
-	char	str[5];
-//	if (!(str = (char *)malloc(sizeof(char) * 4)))
-//		return ;
+	char	*str;
+	if (!(str = (char *)malloc(sizeof(char) * 5)))
+		return ;
+	str[4] = '\0';
 	ft_putendl("entrer une instruction");
 	while (fgets(str, sizeof str, stdin))
 	{
@@ -50,11 +81,6 @@ int			check_av(int ac, char **av)
 			y = 0;
 			while (av[x][y] || av[x][y] != ' ' || av[x][y] != '\n')
 			{
-				ft_putnbr(x);
-				ft_putnbr(y);
-				ft_putstr(" arguments [");
-				ft_putchar(av[x][y]);
-				ft_putstr("]\n");
 				if (ft_isdigit(av[x][y]))
 				{
 					y++;
@@ -69,8 +95,6 @@ int			check_av(int ac, char **av)
 			}
 			//probleme avec atoi, il transforme les int overflow en int
 			nb[x - 1] = ft_atoi(av[x]);
-			ft_putnbr(nb[x - 1]);
-			ft_putchar('\n');
 			if (nb[x - 1] < INT_MIN || nb[x - 1] > INT_MAX)
 			{
 				ft_putendl("Error");
