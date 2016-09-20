1c03f118bbd0ad0c21a175ec4dd3d726b06a8a7f
Stephane MARTINS <stmartin@e1r10p20.42.fr>
2016-09-20 19:42:37 +0200

    add do_sa test in push_swap.c

--- src2/push_swap.c
+++ src2/push_swap.c
@@ -6,17 +6,19 @@
 /*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/20 19:02:15 by stmartin          #+#    #+#             */
-/*   Updated: 2016/09/20 19:17:26 by stmartin         ###   ########.fr       */
+/*   Updated: 2016/09/20 19:41:58 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
-void		move_nb(t_data *pilea, t_data *pileb)
+void		move_nb(t_data **pilea, t_data **pileb)
 {
-	(void)pilea;
+	if (*pilea && (*pilea)->next)
+	{
+		if ((*pilea)->nb > (*pilea)->next->nb)
+			do_sa(*pilea);
+	}
 	(void)pileb;
-
-	ft_putstr("entre");
 }
 
 void		push_swap(t_data *pilea, t_data *pileb)
@@ -24,16 +26,30 @@ void		push_swap(t_data *pilea, t_data *pileb)
 	t_data		*tmp;
 
 	tmp = pilea;
+		ft_putendl("liste des nombres :");
 	while (tmp)
 	{
 		ft_putnbr(tmp->nb);
 		ft_putchar('\n');
+		tmp = tmp->next;
+	}
+	tmp = pilea;
+	while (tmp)
+	{
 		if (tmp->next && tmp->nb > tmp->next->nb)
 		{
-			move_nb(pilea, pileb);
+			move_nb(&pilea, &pileb);
 			tmp = pilea;
 		}
 		else
 			tmp = tmp->next;
+	}
+	tmp = pilea;
+		ft_putendl("apres traitement :");
+	while (tmp)
+	{
+		ft_putnbr(tmp->nb);
+		ft_putchar('\n');
+		tmp = tmp->next;
 	}
 }
