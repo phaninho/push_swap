bf0b1eb3e2454178f34522216b19e984a2cc1e7a
Stephane MARTINS <stmartin@e1r10p20.42.fr>
2016-09-20 19:18:11 +0200

    add move_nb, nothing on it

--- src2/push_swap.c
+++ src2/push_swap.c
@@ -6,18 +6,34 @@
 /*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/20 19:02:15 by stmartin          #+#    #+#             */
-/*   Updated: 2016/09/20 19:04:37 by stmartin         ###   ########.fr       */
+/*   Updated: 2016/09/20 19:17:26 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
-void				push_swap(t_data *pilea, t_data *pileb)
+void		move_nb(t_data *pilea, t_data *pileb)
 {
+	(void)pilea;
 	(void)pileb;
-	while (pilea)
+
+	ft_putstr("entre");
+}
+
+void		push_swap(t_data *pilea, t_data *pileb)
+{
+	t_data		*tmp;
+
+	tmp = pilea;
+	while (tmp)
 	{
-		ft_putnbr(pilea->nb);
+		ft_putnbr(tmp->nb);
 		ft_putchar('\n');
-		pilea = pilea->next;
+		if (tmp->next && tmp->nb > tmp->next->nb)
+		{
+			move_nb(pilea, pileb);
+			tmp = pilea;
+		}
+		else
+			tmp = tmp->next;
 	}
 }
